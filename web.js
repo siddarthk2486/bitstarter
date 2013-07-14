bac9303e7ba63ea40eec8e7cf97382949b5641c1
siddarthk2486 <sidk949@msn.com>
2013-07-14 00:46:21 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var msg=fs.readFileSync ("index.html",true);
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
